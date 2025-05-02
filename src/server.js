90574244dc72672d87ea6f93c3c5b43d76d3654a
4drianC4 <camararequeadrianrafael@gmail.com>
2025-05-02 19:20:17 -0400

    diseño de arquitectura y orden de archivos

---
+++ src/server.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const morgan = require('morgan');
+const cors = require('cors');
+const path = require('path');
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(morgan('dev'));
+
+const port = process.env.PORT || 5000;
+app.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
+    });
