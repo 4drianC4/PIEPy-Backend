42d306b47fcaefeeea1e42e09a99ee4d6c6647ff
4drianC4 <camararequeadrianrafael@gmail.com>
2025-05-05 22:14:26 -0400

    feat: agregado de modelo contenidos

---
+++ src/models/Contenidos.js
@@ -0,0 +1,33 @@
+'use strict';
+
+module.exports = (sequelize, DataTypes) => {
+    const Contenidos = sequelize.define('contenidos', {
+        id: {
+            type: DataTypes.INTEGER,
+            primaryKey: true,
+            autoIncrement: true
+        },
+        idCursos: {
+            type: DataTypes.INTEGER,
+            references: {
+                model: 'cursos',
+                key: 'id'
+            }
+        },
+        descripcion: {
+            type: DataTypes.STRING,
+            allowNull: false
+        },
+        informacion: {
+            type: DataTypes.TEXT,
+            allowNull: false
+        },
+    }, {
+        tableName: 'contenidos',
+        timestamps: false
+    });
+    Contenidos.associate = function(models) {
+        Contenidos.belongsTo(models.cursos, { foreignKey: 'idCursos' });
+    }
+    return Contenidos;
+};  
