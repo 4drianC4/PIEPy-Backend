d5f3bbfc8bdb0a6bc0298aa821a3792243c8a656
4drianC4 <camararequeadrianrafael@gmail.com>
2025-05-14 01:19:40 -0400

    feat: analizador de codigo

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -2,7 +2,9 @@ const express = require('express');
 const router = express.Router();
 
 const cursoRoutes = require('./CursoRoutes');
+const analizadorRoutes = require('./AnalizadorRoutes');
 
 router.use('/cursos', cursoRoutes);
+router.use('/analizador', analizadorRoutes);
 
 module.exports = router;
