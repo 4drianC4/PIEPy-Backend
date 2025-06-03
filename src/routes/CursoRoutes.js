86bfd1151cbaa3bb2847d342e6cc816e87886369
Zer0-jpg <kevin-wal@hotmail.com>
2025-06-02 21:23:58 -0400

    ocultar-cursos

--- src/routes/CursoRoutes.js
+++ src/routes/CursoRoutes.js
@@ -8,14 +8,6 @@ router.get('/', cursoController.getAllCursos);
 router.get('/ocultos/:idAlumno', cursoController.getCursosOcultosParaEstudiante);
 router.get('/disponibles/:idAlumno', cursoController.getCursosDisponiblesParaEstudiante);
 
-// Rutas para manejar casos sin idAlumno - devuelven 404
-router.get('/ocultos/', (req, res) => {
-    res.status(404).json({ message: 'ID de alumno requerido' });
-});
-router.get('/disponibles/', (req, res) => {
-    res.status(404).json({ message: 'ID de alumno requerido' });
-});
-
 // Ruta genérica (DEBE IR DESPUÉS de las rutas específicas)
 router.get('/:id', cursoController.getCursoById);
 
