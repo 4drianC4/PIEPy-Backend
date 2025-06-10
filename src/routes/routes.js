658633fa788559c35f754dfa8ed869362ce5da4c
alastorr22 <marcomontesgarnica@gmail.com>
2025-06-10 04:39:13 -0400

    feat: agregar rutas para practicas

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -3,8 +3,13 @@ const router = express.Router();
 
 const cursoRoutes = require('./CursoRoutes');
 const analizadorRoutes = require('./AnalizadorRoutes');
+const moduloAprendizajeRoutes = require('./ModuloAprendizajeRoutes');
+const practicaRoutes = require('./PracticaRoutes');
+const practicaCamposRoutes = require('./PracticaCamposRoutes'); 
 
 router.use('/cursos', cursoRoutes);
 router.use('/analizador', analizadorRoutes);
-
+router.use('/modulos-aprendizaje', moduloAprendizajeRoutes);
+router.use('/practicas', practicaRoutes);
+router.use('/practica-campos', practicaCamposRoutes); 
 module.exports = router;
