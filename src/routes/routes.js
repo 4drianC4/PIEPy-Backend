86975d8071a8abdc42c46565ef7a2fb40c2e1d82
alastorr22 <marcomontesgarnica@gmail.com>
2025-06-10 04:42:40 -0400

    feat: agregar rutas para certificaciones y evaluaciones

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -3,13 +3,18 @@ const router = express.Router();
 
 const cursoRoutes = require('./CursoRoutes');
 const analizadorRoutes = require('./AnalizadorRoutes');
+const certificacionRoutes = require('./CertificacionRoutes');
+const evaluacionRoutes = require('./EvaluacionRoutes');
 const moduloAprendizajeRoutes = require('./ModuloAprendizajeRoutes');
 const practicaRoutes = require('./PracticaRoutes');
 const practicaCamposRoutes = require('./PracticaCamposRoutes'); 
 
 router.use('/cursos', cursoRoutes);
 router.use('/analizador', analizadorRoutes);
+router.use('/certificaciones', certificacionRoutes);
+router.use('/evaluaciones', evaluacionRoutes);
 router.use('/modulos-aprendizaje', moduloAprendizajeRoutes);
 router.use('/practicas', practicaRoutes);
 router.use('/practica-campos', practicaCamposRoutes); 
+
 module.exports = router;
